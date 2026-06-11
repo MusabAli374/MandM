822796a19a7602d18b044a1ce563935e56d7f56e
musabali314 <25100190@lums.edu.pk>
2026-06-12 00:00:35 +0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -125,7 +125,7 @@ function updateCarousel() {
     lily.position.y = offset === 0 ? 0 : -0.35;
     lily.position.z = Math.abs(offset) * -1.7;
 
-    const scale = offset === 0 ? 0.45 : 0.25;
+    const scale = offset === 0 ? 0.035 : 0.025;
     lily.scale.set(scale, scale, scale);
 
     lily.rotation.y = offset * 0.45;
